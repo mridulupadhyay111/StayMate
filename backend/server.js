b16ffb72221a99b2f1904f4781fc738e9c1916e4
mridulupadhyay111 <mridulsupadhyay333@gmail.com>
2026-04-05 12:17:17 +0530

    issue fixed

--- backend/server.js
+++ backend/server.js
@@ -41,8 +41,8 @@ mongoose
     process.exit(1);
   });
 
-app.use(express.static(path.join(__dirname, "frontend/build")));
+app.use(express.static(path.join(__dirname, "frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/build/index.html"));
+  res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
 });
